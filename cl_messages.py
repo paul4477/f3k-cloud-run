fb04d5a945a62a38179a82336dac9a48d5edf2f4
Paul Robinson <paul.d.robinson+git@gmail.com>
2025-10-11 08:48:42 +0100

    Reverted simplequeue to queue

--- cl_messages.py
+++ cl_messages.py
@@ -6,7 +6,7 @@ class MessageAnnouncer:
         self.listeners = []
 
     def listen(self):
-        q = queue.SimpleQueue(maxsize=5)
+        q = queue.Queue(maxsize=50)
         self.listeners.append(q)
         return q
 
